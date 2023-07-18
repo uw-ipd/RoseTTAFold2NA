436d75f182f93236b061aef7b4b1a1d013ec83b4
Alex Morehead <alex.morehead@gmail.com>
2023-07-18 14:44:37 -0500

    Retain full FASTA filepath when parsing Bash script inputs

--- run_RF2NA.sh
+++ run_RF2NA.sh
@@ -78,8 +78,9 @@ for i in "$@"
 do
     type=`echo $i | awk -F: '{if (NF==1) {print "P"} else {print $1}}'`
     type=${type^^}
-    fasta=`echo $i | awk -F: '{if (NF==1) {print $1} else {print $2}}'`
-    tag=`basename $fasta | sed -E 's/\.fasta$|\.fas$|\.fa$//'`
+    fasta=`echo $i`
+    fasta_name=`echo $i | awk -F: '{if (NF==1) {print $1} else {print $2}}'`
+    tag=`basename $fasta_name | sed -E 's/\.fasta$|\.fas$|\.fa$//'`
     type=`basename $type`  # extract only the last component after the last '/', so users can pass in an absolute path for each FASTA
 
     if [ $type = 'P' ]
